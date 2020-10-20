201331965783679e356b84aab2d1ab6747dea585
Lukas Simianer <lsimianer@gmail.com>
2020-10-20 14:40:18 -0500

    Updates

---
+++ precache-manifest.0a85be15de15a539cbda0053e65f605a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4a420dfd7df8c60c5fd9c7ee8679970",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "73f03a361711197c5b8c",
+    "url": "/life/static/css/main.8a600622.chunk.css"
+  },
+  {
+    "revision": "9ff9ea30426c57a41c67",
+    "url": "/life/static/js/2.2e38a82f.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/life/static/js/2.2e38a82f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f03a361711197c5b8c",
+    "url": "/life/static/js/main.0c2a4f9e.chunk.js"
+  },
+  {
+    "revision": "39b6448f48d3fd565aec",
+    "url": "/life/static/js/runtime-main.26cbc897.js"
+  }
+]);
